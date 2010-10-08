329ae0bbb55a813b035de6d5f975c30ca6541cb6
Paul Brower <brower.paul@gmail.com>
2010-10-07 23:17:03 -0400

    init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  include Authentication
+  helper :all # include all helpers, all the time
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  # Scrub sensitive parameters from your log
+  # filter_parameter_logging :password
+
+  def logged_in
+    redirect_to "/" unless !current_user.nil?
+  end
+end
